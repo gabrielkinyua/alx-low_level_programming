f2481d7b439d3830b6c62bab1c10f64a8198920f
gabrielkinyua <gabzkinyua@gmail.com>
2023-02-19 22:14:46 +0000

    Check for whether the number n is + or -

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,31 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <time.h>
 /**
- * main - prints of the number is positive,negative or 0
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
 	{
-		printf("%d is zero\n", n);
-	} 
-	else if(n == 0)
+		printf("%d", n);
+	}
+	if (n > 0)
 	{
-		printf("%d is zero\n", n);
-	} 
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
 	else
 	{
-		printf("%d is positive\n", n);
+		printf("%d is negative", n);
 	}
 	return (0);
 }
