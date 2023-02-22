1050f89f494fea1a614781d4b4ac03e23eb3c1f3
gabrielkinyua <gabzkinyua@gmail.com>
2023-02-22 18:20:05 +0000

    Add a program that prints putchar_

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
