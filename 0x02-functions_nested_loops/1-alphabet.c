edacf4c04a94bec6b4e096e4950ff0375dc11f73
gabrielkinyua <gabzkinyua@gmail.com>
2023-02-22 19:25:23 +0000

    Add a file that prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (1 = 97; i <= 122 i++ )
+	{
+		putchar('i');
+	}
+	putchar('\n');
+;
+
+	
