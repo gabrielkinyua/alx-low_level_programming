6b4a759698c908180257bd5b33002f23158b7ddf
gabrielkinyua <gabzkinyua@gmail.com>
2023-02-28 19:39:45 +0000

    Function that calculates length if a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int_strlen - calculate length of a string
+ * @s:string
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++);
+	return i;
+}
