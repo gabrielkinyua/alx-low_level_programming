c876e7d6d09b5e87a7c1e75d39cd2d03510f9ff9
gabrielkinyua <gabzkinyua@gmail.com>
2023-03-07 21:08:32 +0000

    Add basic C source file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
- * _strchr - locates a character in a string
- * @s: string containing the character
- * @c: character to be located in the string
- *
- * Return: Always 0
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
 	}
 	return (0);
 }
-
