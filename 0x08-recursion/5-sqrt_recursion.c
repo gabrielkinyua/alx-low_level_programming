074550a9083485297ddb41062deb3f72cc83a7ad
gabrielkinyua <gabzkinyua@gmail.com>
2023-03-08 23:10:06 +0000

    compile

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - gets square root of a number
+ * @n: input
+ *
+ * Return: Always 0
+ */
+int _sqrt_recursion(int n)
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if input integer is a prime number
+ * @n: input
+ *
+ * Return: Always 0
+ */
+int is_prime_number(int n)
+{
+
