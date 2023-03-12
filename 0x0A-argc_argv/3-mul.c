44a87007c663974a8e51c437bd5a747fbf5c912a
gabrielkinyua <gabzkinyua@gmail.com>
2023-03-12 19:02:58 +0000

    Add a basic C source file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies 2 numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	
+	if(argc != 3)
+	{	
+		printf("Error\n");
+		return(1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+	printf("%d\n", result);
+	return(0);
+}
+
+
+
